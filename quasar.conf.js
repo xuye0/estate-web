54486ac32d24a9e29701a200e5d2a829c32d7ca0
xuye <404022186@qq.com>
2021-12-04 18:13:58 +0800

    fix

--- quasar.conf.js
+++ quasar.conf.js
@@ -81,7 +81,7 @@ module.exports = configure(function (/* ctx */) {
         },
       https: false,
       port: 8081,
-      open: false // opens browser window automatically
+      open: true // opens browser window automatically
     }},
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
