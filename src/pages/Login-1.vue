91d082f592c421e48676d607c153ad6d38911f37
xuye <xuye@shenqingtech.com>
2022-05-22 08:03:24 +0800

    avatar

--- src/pages/Login-1.vue
+++ src/pages/Login-1.vue
@@ -6,8 +6,10 @@
           v-bind:style="$q.screen.lt.sm ? { width: '80%' } : { width: '30%' }"
         >
           <q-card-section>
-            <q-avatar size="103px" class="absolute-center shadow-10">
-              <img src="profile.svg" />
+            <q-avatar size="100px" class="absolute-center">
+              <img
+                src="https://estate-xuye.oss-cn-shanghai.aliyuncs.com/user.png"
+              />
             </q-avatar>
           </q-card-section>
           <q-card-section>
